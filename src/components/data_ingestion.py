7478c9cff8597816e49fdeed95ed20ed08a842f3
shubham <thakur555666@gmail.com>
2025-04-01 11:06:40 +0530

    preprocessing

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformationConfig, DataTransformation
 
 @dataclass  # Decorator - dataclass will directly define our class variable, no need to create __init__
 class DataIngestionConfig:
@@ -42,3 +43,11 @@ class DataIngestion:
             )
         except Exception as e:
             raise CustomException(e, sys)
+
+
+if __name__=='__main__':
+    obj = DataIngestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
