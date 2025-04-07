bd69b985589b6647b5836df80a1e7fd026d77c12
Shubham Singh <shubham47047@gmail.com>
2025-04-07 17:42:58 +0530

    Prediction pipeline

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, render_template
+import numpy as np
+import pandas as pd
+
+from sklearn.preprocessing import StandardScaler
+from src.pipeline.predict_pipeline import PredictPipeline, CustomData
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predictdata',methods = ['GET','POST'])
+def predict_datapoint():
+    if request.method == 'GET':
+        return render_template('home.html')
+    else:
+        data = CustomData(
+            gender=request.form.get('gender'),
+            race_ethnicity=request.form.get('ethnicity'),
+            parental_level_of_education=request.form.get('parental_level_of_education'),
+            lunch=request.form.get('lunch'),
+            test_preparation_course=request.form.get('test_preparation_course'),
+            reading_score= float(request.form.get('writing_score')),
+            writing_score=float(request.form.get('reading_score')))
+
+        prediction_df = data.get_data_as_dataframe()
+        print(prediction_df)
+        print("Before Prediction")
+
+        prediction_pipeline = PredictPipeline()
+        print("Doing Prediction")
+        results = prediction_pipeline.predict(prediction_df)
+        print("Completed prediction")
+        return render_template('home.html',results = results[0])
--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import pandas as pd
 
 from sklearn.metrics import r2_score
 from src.exception import CustomException
+from sklearn.model_selection import RandomizedSearchCV
 
 import pickle
 
@@ -22,12 +23,18 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
 
 
-def evaluate_models(X_train, y_train,X_test,y_test,models):
+def evaluate_models(X_train, y_train,X_test,y_test,models,params):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para = params[list(models.keys())[i]]
+
+            random_search = RandomizedSearchCV(model,para,cv= 3)
+            random_search.fit(X_train,y_train)
+
+            model.set_params(**random_search.best_params_)
             model.fit(X_train,y_train)
 
             y_train_pred = model.predict(X_train)
@@ -42,5 +49,13 @@ def evaluate_models(X_train, y_train,X_test,y_test,models):
 
         return report
 
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+def load_model(file_path):
+    try:
+        with open(file_path, "r") as file_obj:
+            return pickle.load(file_obj)
     except Exception as e:
         raise CustomException(e, sys)
