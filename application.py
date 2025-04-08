07a1fd863a29fbf8d439ea434c28cf6b53489973
shubham <thakur555666@gmail.com>
2025-04-08 13:29:30 +0530

    Deployment Pipeline

--- application.py
+++ application.py
@@ -1,17 +1,17 @@
 from flask import Flask, request, render_template
-import numpy as np
-import pandas as pd
-
-from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import PredictPipeline, CustomData
 
-app = Flask(__name__)
+application = Flask(__name__)
+
+app = application
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/predictdata',methods = ['GET','POST'])
+
+@app.route('/predictdata', methods=['GET', 'POST'])
 def predict_datapoint():
     if request.method == 'GET':
         return render_template('home.html')
@@ -22,7 +22,7 @@ def predict_datapoint():
             parental_level_of_education=request.form.get('parental_level_of_education'),
             lunch=request.form.get('lunch'),
             test_preparation_course=request.form.get('test_preparation_course'),
-            reading_score= float(request.form.get('writing_score')),
+            reading_score=float(request.form.get('writing_score')),
             writing_score=float(request.form.get('reading_score')))
 
         prediction_df = data.get_data_as_dataframe()
@@ -33,8 +33,8 @@ def predict_datapoint():
         print("Doing Prediction")
         results = prediction_pipeline.predict(prediction_df)
         print("Completed prediction")
-        return render_template('home.html',results = results[0])
+        return render_template('home.html', results=results[0])
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",debug=True)
+    app.run(host="0.0.0.0")
