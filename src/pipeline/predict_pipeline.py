991b198188b638450f33ce0b14748a7ef249f693
Shubham Singh <shubham47047@gmail.com>
2025-04-08 18:14:49 +0530

    updated application

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -30,8 +30,8 @@ class CustomData:
                  parental_level_of_education,
                  lunch: str,
                  test_preparation_course: str,
-                 reading_score: int,
-                 writing_score: int):
+                 reading_score: float,
+                 writing_score: float):
 
         self.gender = gender
 
